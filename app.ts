6f56b1b7aaa2a1ecbf7bba380ecde0567f344cd6
Robison Pereira Machado <robisonpmachado@gmail.com>
2022-11-12 18:20:57 -0300

    Seção 3: declaração de variáveis, configuração e primeiro exemplo

---
+++ app.ts
@@ -0,0 +1,17 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+
+let episode: number = 4
+
+
+let favoriteDroid: string
+favoriteDroid = 'BB-8'
+
+
+
+console.log(message)
+
+console.log("This is episode "+4);
+episode = episode+1
+console.log("Next episode is "+episode);
+
+console.log("My favorite droid is "+favoriteDroid);
