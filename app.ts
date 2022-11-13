135b3a8b4b0f8a505e96af4d57e321a96e626b91
Robison Pereira Machado <robisonpmachado@gmail.com>
2022-11-12 21:12:23 -0300

    Seção 3: organizando os exemplos em módulos

--- app.ts
+++ app.ts
@@ -1,40 +1,17 @@
-class Spacecraft{
-    constructor(public propulsor: string){
+import { Spacecraft, Containership } from './base-ships'
+import { MilleniumFalcon } from './starfighters'
 
-    }
 
-    jumpIntoHyperspace(){
-        console.log(`Entering hyperspace with ${this.propulsor}`);
-    }
-}
 
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
-class MilleniumFalcon extends Spacecraft implements Containership{
-    cargoContainers: number
 
-    constructor(){
-        super('hyperdrive')
-        this.cargoContainers = 4
-    }
-
-
-    jumpIntoHyperspace() {
-        if(Math.random() >= 0.5){
-            super.jumpIntoHyperspace()
-        }else{
-            console.log('Failed to jump into hyperspace');
-        }
-    }
-}
 
 let falcon = new MilleniumFalcon()
 falcon.jumpIntoHyperspace()
 
-interface Containership{
-    cargoContainers: number;
-}
+
 
 let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2
 
---
+++ secao 3 - usando classes e interfaces/app.ts
@@ -0,0 +1,48 @@
+function main2(){
+
+    class Spacecraft{
+        constructor(public propulsor: string){
+    
+        }
+    
+        jumpIntoHyperspace(){
+            console.log(`Entering hyperspace with ${this.propulsor}`);
+        }
+    }
+    
+    let ship = new Spacecraft('hyperdrive')
+    ship.jumpIntoHyperspace()
+    
+    class MilleniumFalcon extends Spacecraft implements Containership{
+        cargoContainers: number
+    
+        constructor(){
+            super('hyperdrive')
+            this.cargoContainers = 4
+        }
+    
+    
+        jumpIntoHyperspace() {
+            if(Math.random() >= 0.5){
+                super.jumpIntoHyperspace()
+            }else{
+                console.log('Failed to jump into hyperspace');
+            }
+        }
+    }
+    
+    let falcon = new MilleniumFalcon()
+    falcon.jumpIntoHyperspace()
+    
+    interface Containership{
+        cargoContainers: number;
+    }
+    
+    let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2
+    
+    console.log(`Is falcon good for the job? ${ goodForTheJob(falcon) ? 'YES' : 'NO' }`);    
+
+}
+
+main2()
+
